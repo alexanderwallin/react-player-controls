1b92437d85fa59b39fed415b7db5190a808ac360
Alexander Wallin <office@alexanderwallin.com>
2016-06-13 12:21:26 +0100

    Adds missing exports

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import PauseButton from './components/PauseButton.js'
 import NextButton from './components/NextButton.js'
 import PrevButton from './components/PrevButton.js'
 
+import ProgressBar from './components/ProgressBar.js'
+import FormattedTime from './components/FormattedTime.js'
+
 export {
   PlaybackControls,
   PlayButton,
   PauseButton,
   NextButton,
   PrevButton,
+
+  ProgressBar,
+  FormattedTime,
 }
