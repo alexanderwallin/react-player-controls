5e6aee388573a072585c6ff3b726aa9f655d96ba
Alexander Wallin <office@alexanderwallin.com>
2016-07-06 12:46:46 +0100

    Introduces a VolumeSlider component

---
+++ src/components/VolumeSlider.js
@@ -0,0 +1,104 @@
+import React, { Component, PropTypes } from 'react'
+import autobind from 'autobind-decorator'
+import classNames from 'classnames'
+
+import RangeControlOverlay, { ControlDirection } from './RangeControlOverlay.js'
+
+const { number, bool, func, string } = PropTypes
+
+/**
+ * Volume slider component
+ */
+class VolumeSlider extends Component {
+
+  static propTypes = {
+    volume: number,
+    isEnabled: bool,
+    onVolumeChange: func,
+    extraClasses: string,
+  }
+
+  static defaultProps = {
+    volume: 0,
+    isEnabled: false,
+    onVolumeChange: () => {},
+    extraClasses: '',
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.sliderEl = null
+
+    this.state = {
+      currentIntent: 0,
+    }
+  }
+
+  @autobind
+  storeRef (rootEl) {
+    this.sliderEl = rootEl
+  }
+
+  @autobind
+  getBounds () {
+    return this.sliderEl.getBoundingClientRect()
+  }
+
+  @autobind
+  handleVolumeChange (volume) {
+    const { isEnabled, onVolumeChange } = this.props
+
+    if (isEnabled) {
+      onVolumeChange(volume)
+    }
+  }
+
+  @autobind
+  handleIntent (volume) {
+    const intent = this.props.isEnabled ? volume : 0
+
+    this.setState({
+      currentIntent: intent,
+    })
+  }
+
+  render () {
+    const { volume, isEnabled, extraClasses } = this.props
+    const { currentIntent } = this.state
+
+    const volumePercentage = Math.min(100, Math.max(0, volume * 100))
+    const styleBottom = `${volumePercentage}%`
+
+    const appliedIntent = isEnabled && currentIntent !== 0 ? currentIntent : 0
+    const isDecreaseIntent = appliedIntent && currentIntent < volume
+
+    return (
+      <div
+        className={classNames('VolumeSlider', extraClasses, {
+          isEnabled,
+          isDecreaseIntent,
+        })}
+        ref={this.storeRef}
+      >
+        <div className="VolumeSlider-value" style={{ height: styleBottom }} />
+
+        <div className="VolumeSlider-intent" style={{ height: `${appliedIntent * 100}%` }} />
+
+        <div className="VolumeSlider-handle" style={{ bottom: styleBottom }} />
+
+        {isEnabled && (
+          <RangeControlOverlay
+            extraClasses="VolumeSlider-seek"
+            bounds={this.getBounds}
+            direction={ControlDirection.VERTICAL}
+            onValue={this.handleVolumeChange}
+            onIntent={this.handleIntent}
+          />
+        )}
+      </div>
+    )
+  }
+}
+
+export default VolumeSlider
---
+++ tests/VolumeSlider-test.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import { spy } from 'sinon'
+
+chai.use(chaiEnzyme())
+
+import VolumeSlider, { ControlDirection } from '../src/components/VolumeSlider.js'
+import RangeControlOverlay from '../src/components/RangeControlOverlay.js'
+
+describe('<VolumeSlider />', () => {
+
+  it('renders a value with correct height', () => {
+    const slider = shallow(<VolumeSlider volume={0.6} />)
+    const value = slider.find('.VolumeSlider-value')
+
+    expect(value.props().style).to.eql({ height: '60%' })
+  })
+
+  it('renders an intent with correct height', () => {
+    const slider = shallow(<VolumeSlider isEnabled={false} volume={0.45} />)
+
+    expect(slider.find('.VolumeSlider-intent').props().style.height).to.equal('0%')
+
+    // Disabled, so should still be 0%
+    slider.setState({ currentIntent: 0.2 })
+    expect(slider.find('.VolumeSlider-intent').props().style.height).to.equal('0%')
+
+    slider.setProps({ ...slider.props(), isEnabled: true })
+    expect(slider.find('.VolumeSlider-intent').props().style.height).to.equal('20%')
+
+    slider.setState({ currentIntent: 0.91 })
+    expect(slider.find('.VolumeSlider-intent').props().style.height).to.equal('91%')
+  })
+
+  it('renders a <RangeControlOverlay /> only when enabled', () => {
+    const slider = shallow(<VolumeSlider isEnabled={false} />)
+    expect(slider.find(RangeControlOverlay).length).to.equal(0)
+
+    slider.setProps({ isEnabled: true })
+    expect(slider.find(RangeControlOverlay).length).to.equal(1)
+  })
+
+  it('provides <RangeControlOverlay /> with a valid bounds', () => {
+    const slider = mount(<VolumeSlider isEnabled />)
+    const bounds = slider.instance().getBounds()
+
+    expect(bounds).to.be.ok
+    expect(bounds).to.have.all.keys(['bottom', 'height', 'left', 'right', 'top', 'width'])
+  })
+
+})
