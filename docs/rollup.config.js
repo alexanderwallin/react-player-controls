0cad0c7c9e5e83e1c3aa6c46bf3b227b70215651
Alexander Wallin <office@alexanderwallin.com>
2016-08-23 15:37:10 +0100

    Switches from rollup to browserify bundling

--- docs/rollup.config.js
+++
@@ -1,8 +0,0 @@
-import babel from 'rollup-plugin-babel'
-
-module.exports = {
-  entry: 'src/js/demos.js',
-  dest: 'build/scripts.js',
-  plugins: [babel()],
-  format: 'iife',
-}
--- docs/src/js/demos.js
+++ docs/src/js/demos.js
@@ -1,1 +1,8 @@
-console.log('react-player-controls')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { PlayButton } from '../../../dist/index.js'
+
+const DemoPlayButton = () =>
+  <div>Demo play button</div>
+
+ReactDOM.render(<DemoPlayButton />, document.querySelector('.component-demo'))
