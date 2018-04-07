ccf7b1789b9bcfff4162d24f09fdddea23e93159
Alexander Wallin <office@alexanderwallin.com>
2018-04-07 14:25:45 +0100

    (composition) Cleans up index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,13 @@
-export * from './components/icons.js'
-
-import Button from './components/Button.js'
-
-import ProgressBar from './components/ProgressBar.js'
-import FormattedTime from './components/FormattedTime.js'
-import TimeMarker, { TimeMarkerType } from './components/TimeMarker.js'
-
-import VolumeSlider from './components/VolumeSlider.js'
-
 import { Direction } from './constants.js'
-
-import { PlayIcon, PauseIcon, PreviousIcon, NextIcon, SoundOnIcon, SoundOffIcon } from './components/icons.js'
+import Button from './components/Button.js'
+import FormattedTime from './components/FormattedTime.js'
+import * as PlayerIcon from './components/icons.js'
+import Slider from './components/Slider.js'
 
 export {
   Button,
-
-  ProgressBar,
-  FormattedTime,
-  TimeMarker,
-  TimeMarkerType,
-
-  VolumeSlider,
-
   Direction,
-
-  PlayIcon,
-  PauseIcon,
-  PreviousIcon,
-  NextIcon,
-  SoundOnIcon,
-  SoundOffIcon,
+  FormattedTime,
+  PlayerIcon,
+  Slider,
 }
