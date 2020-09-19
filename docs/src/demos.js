dc65d91d7804c524ac05ed45155f5e313f8bc2f1
Alexander Wallin <office@alexanderwallin.com>
2020-09-19 15:31:41 +0200

    Update <Slider> demo

--- docs/src/demos.js
+++ docs/src/demos.js
@@ -1,6 +1,6 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent, useState } from 'react'
 import ReactDOM from 'react-dom'
-// import PropTypes from 'prop-types'
+// // import PropTypes from 'prop-types'
 import Prism from 'prismjs'
 import 'prismjs/components/prism-jsx.js'
 
@@ -34,7 +34,7 @@ class ButtonDemo extends PureComponent {
     return (
       <div className="ComponentDemo ButtonDemo">
         <pre className="ComponentDemo-code">
-          <code className="language-jsx" dangerouslySetInnerHTML={{
+          <code className="language-jsx" dangerouslySetInnerHTMLL={{
             __html: Prism.highlight(
               `<Button\n  isEnabled={this.state.isEnabled}\n  onClick={() => alert('Clicked!')} \n>\n  This is a button\n</Button>`,
               Prism.languages.jsx
@@ -142,21 +142,24 @@ class PlayerIconDemo extends PureComponent {
 //
 const SliderBar = ({ direction, value, style }) => (
   <div
-    style={Object.assign({}, {
+    style={{
       position: 'absolute',
       background: GRAY,
       borderRadius: 4,
-    }, direction === Direction.HORIZONTAL ? {
-      top: 0,
-      bottom: 0,
-      left: 0,
-      width: `${value * 100}%`,
-    } : {
-      right: 0,
-      bottom: 0,
-      left: 0,
-      height: `${value * 100}%`,
-    }, style)}
+      ...(direction === Direction.HORIZONTAL ? {
+        top: 'calc(50% - 4px)',
+        left: 0,
+        width: `${value * 100}%`,
+        height: 8,
+      } : {
+        right: 0,
+        bottom: 0,
+        left: 'calc(50% - 4px)',
+        width: 8,
+        height: `${value * 100}%`,
+      }),
+      ...style,
+    }}
   />
 )
 
@@ -174,12 +177,12 @@ const SliderHandle = ({ direction, value, style }) => (
         transform: 'scale(1.3)',
       }
     }, direction === Direction.HORIZONTAL ? {
-      top: 0,
+      top: 'calc(50% - 4px)',
       left: `${value * 100}%`,
       marginTop: -4,
       marginLeft: -8,
     } : {
-      left: 0,
+      left: 'calc(50% - 4px)',
       bottom: `${value * 100}%`,
       marginBottom: -8,
       marginLeft: -4,
@@ -187,115 +190,107 @@ const SliderHandle = ({ direction, value, style }) => (
   />
 )
 
-class SliderDemo extends PureComponent {
-  constructor (props) {
-    super(props)
+function SliderDemo () {
+  const [isEnabled, setIsEnabled] = useState(true)
+  const [direction, setDirection] = useState(Direction.HORIZONTAL)
+  const [value, setValue] = useState(0)
+  const [lastValueStart, setLastValueStart] = useState(0)
+  const [lastValueEnd, setLastValueEnd] = useState(0)
+  const [lastIntent, setLastIntent] = useState(0)
+  const [lastIntentStart, setLastIntentStart] = useState(0)
+  const [lastIntentEndCount, setLastIntentEndCount] = useState(0)
 
-    this.state = {
-      isEnabled: true,
-      direction: Direction.HORIZONTAL,
-      value: 0,
-      lastValueStart: 0,
-      lastValueEnd: 0,
-      lastIntent: 0,
-      lastIntentStart: 0,
-      lastIntentEndCount: 0,
-    }
-  }
+  return (
+    <div className="ComponentDemo SliderDemo">
+      <pre className="ComponentDemo-code">
+        <code className="language-jsx" dangerouslySetInnerHTML={{
+          __html: Prism.highlight(
+            `const SliderBar = ({ direction, value, style }) => <div style={computedStylesHere} />` +
+            `\nconst SliderHandle = ({ direction, value, style }) => <div style={computedStylesHere} />` +
+            `\n` +
+            `\n<Slider\n  isEnabled={this.state.isEnabled}\n  direction={this.state.direction}\n  onChange={newValue => this.setState(() => ({ value: newValue }))}\n  onChangeStart={startValue => this.setState(() => ({ lastValueStart: startValue }))}\n  onChangeEnd={endValue => this.setState(() => ({ lastValueEnd: endValue }))}\n  onIntent={intent => this.setState(() => ({ lastIntent: intent }))}\n  onIntentStart={intent => this.setState(() => ({ lastIntentStart: intent }))}\n  onIntentEnd={() => this.setState(() => ({ lastIntentEndCount: this.state.lastIntentEndCount + 1 }))}\n  style={sliderStylesHere}\n>` +
+            `\n  <SliderBar\n    direction={this.state.direction}\n    value={1}\n    style={{ background: #eee }}\n  />` +
+            `\n  <SliderBar\n    direction={this.state.direction}\n    value={this.state.value}\n    style={{ background: this.state.isEnabled ? '#72d687' : '#878c88' }}\n  />` +
+            `\n  <SliderBar\n    direction={this.state.direction}\n    value={this.state.lastIntent}\n    style={{ background: 'rgba(0, 0, 0, 0.05)' }}\n  />` +
+            `\n  <SliderHandle\n    direction={this.state.direction}\n    value={this.state.value}\n    style={{ background: this.state.isEnabled ? '#72d687' : '#878c88' }}\n  />` +
+            `\n</Slider>`,
+            Prism.languages.jsx
+          )
+        }} />
+      </pre>
 
-  render () {
-    const { isEnabled, direction, value, lastValueStart, lastValueEnd, lastIntent, lastIntentStart, lastIntentEndCount } = this.state
+      <div className="ComponentDemo-settings">
+        <label>
+          <input type="checkbox" checked={isEnabled} onChange={(evt) => setIsEnabled(!isEnabled)} />
+          <code>isEnabled</code>
+        </label>
 
-    return (
-      <div className="ComponentDemo SliderDemo">
-        <pre className="ComponentDemo-code">
-          <code className="language-jsx" dangerouslySetInnerHTML={{
-            __html: Prism.highlight(
-              `const SliderBar = ({ direction, value, style }) => <div style={computedStylesHere} />` +
-              `\nconst SliderHandle = ({ direction, value, style }) => <div style={computedStylesHere} />` +
-              `\n` +
-              `\n<Slider\n  isEnabled={this.state.isEnabled}\n  direction={this.state.direction}\n  onChange={newValue => this.setState(() => ({ value: newValue }))}\n  onChangeStart={startValue => this.setState(() => ({ lastValueStart: startValue }))}\n  onChangeEnd={endValue => this.setState(() => ({ lastValueEnd: endValue }))}\n  onIntent={intent => this.setState(() => ({ lastIntent: intent }))}\n  onIntentStart={intent => this.setState(() => ({ lastIntentStart: intent }))}\n  onIntentEnd={() => this.setState(() => ({ lastIntentEndCount: this.state.lastIntentEndCount + 1 }))}\n  style={sliderStylesHere}\n>` +
-              `\n  <SliderBar\n    direction={this.state.direction}\n    value={this.state.value}\n    style={{ background: this.state.isEnabled ? '#72d687' : '#878c88' }}\n  />` +
-              `\n  <SliderBar\n    direction={this.state.direction}\n    value={this.state.lastIntent}\n    style={{ background: 'rgba(0, 0, 0, 0.05)' }}\n  />` +
-              `\n  <SliderHandle\n    direction={this.state.direction}\n    value={this.state.value}\n    style={{ background: this.state.isEnabled ? '#72d687' : '#878c88' }}\n  />` +
-              `\n</Slider>`,
-              Prism.languages.jsx
-            )
-          }} />
-        </pre>
+        <label>
+          <code>direction</code>
+          <select value={direction} onChange={(evt) => setDirection(evt.target.value)}>
+            <option value={Direction.HORIZONTAL}>Direction.HORIZONTAL</option>
+            <option value={Direction.VERTICAL}>Direction.VERTICAL</option>
+          </select>
+        </label>
 
-        <div className="ComponentDemo-settings">
-          <label>
-            <input type="checkbox" checked={isEnabled} onChange={(evt) => this.setState({ isEnabled: !isEnabled })} />
-            <code>isEnabled</code>
-          </label>
+        <label>
+          <code>value</code>
+          <input type="number" value={value} min={0} max={1} step={0.01} onChange={evt => setValue(evt.target.value)} />
+        </label>
 
-          <label>
-            <code>direction</code>
-            <select value={direction} onChange={(evt) => this.setState({ direction: evt.target.value })}>
-              <option value={Direction.HORIZONTAL}>Direction.HORIZONTAL</option>
-              <option value={Direction.VERTICAL}>Direction.VERTICAL</option>
-            </select>
-          </label>
+        <label>
+          <code>lastValueStart</code>
+          <input type="number" disabled value={lastValueStart} />
+        </label>
 
-          <label>
-            <code>value</code>
-            <input type="number" value={value} min={0} max={1} step={0.01} onChange={evt => this.setState({ value: evt.target.value })} />
-          </label>
+        <label>
+          <code>lastValueEnd</code>
+          <input type="number" disabled value={lastValueEnd} />
+        </label>
 
-          <label>
-            <code>lastValueStart</code>
-            <input type="number" disabled value={lastValueStart} />
-          </label>
+        <label>
+          <code>lastIntent</code>
+          <input type="number" disabled value={lastIntent} />
+        </label>
 
-          <label>
-            <code>lastValueEnd</code>
-            <input type="number" disabled value={lastValueEnd} />
-          </label>
+        <label>
+          <code>lastIntentStart</code>
+          <input type="number" disabled value={lastIntentStart} />
+        </label>
 
-          <label>
-            <code>lastIntent</code>
-            <input type="number" disabled value={lastIntent} />
-          </label>
-
-          <label>
-            <code>lastIntentStart</code>
-            <input type="number" disabled value={lastIntentStart} />
-          </label>
-
-          <label>
-            <code>lastIntentEndCount</code>
-            <input type="number" disabled value={lastIntentEndCount} />
-          </label>
-        </div>
-
-        <div className="ComponentDemo-results">
-          <Slider
-            isEnabled={isEnabled}
-            direction={direction}
-            onChange={newValue => this.setState(() => ({ value: newValue }))}
-            onChangeStart={startValue => this.setState(() => ({ lastValueStart: startValue }))}
-            onChangeEnd={endValue => this.setState(() => ({ lastValueEnd: endValue }))}
-            onIntent={intent => this.setState(() => ({ lastIntent: intent }))}
-            onIntentStart={intent => this.setState(() => ({ lastIntentStart: intent }))}
-            onIntentEnd={() => this.setState(() => ({ lastIntentEndCount: lastIntentEndCount + 1 }))}
-            style={{
-              width: direction === Direction.HORIZONTAL ? 200 : 8,
-              height: direction === Direction.HORIZONTAL ? 8 : 130,
-              borderRadius: 4,
-              background: WHITE_SMOKE,
-              transition: direction === Direction.HORIZONTAL ? 'width 0.1s' : 'height 0.1s',
-              cursor: isEnabled === true ? 'pointer' : 'default',
-            }}
-          >
-            <SliderBar direction={direction} value={value} style={{ background: isEnabled ? GREEN : GRAY }} />
-            <SliderBar direction={direction} value={lastIntent} style={{ background: 'rgba(0, 0, 0, 0.05)' }} />
-            <SliderHandle direction={direction} value={value} style={{ background: isEnabled ? GREEN : GRAY }} />
-          </Slider>
-        </div>
+        <label>
+          <code>lastIntentEndCount</code>
+          <input type="number" disabled value={lastIntentEndCount} />
+        </label>
       </div>
-    )
-  }
+
+      <div className="ComponentDemo-results">
+        <Slider
+          isEnabled={isEnabled}
+          direction={direction}
+          onChange={newValue => setValue(newValue)}
+          onChangeStart={startValue => setLastValueStart(startValue)}
+          onChangeEnd={endValue => setLastValueEnd(endValue)}
+          onIntent={intent => setLastIntent(intent)}
+          onIntentStart={intent => setLastIntentStart(intent)}
+          onIntentEnd={() => setLastIntentEndCount(lastIntentEndCount + 1)}
+          style={{
+            width: direction === Direction.HORIZONTAL ? 200 : 24,
+            height: direction === Direction.HORIZONTAL ? 24 : 130,
+            // borderRadius: 4,
+            // background: WHITE_SMOKE,
+            transition: direction === Direction.HORIZONTAL ? 'width 0.1s' : 'height 0.1s',
+            cursor: isEnabled === true ? 'pointer' : 'default',
+          }}
+        >
+          <SliderBar direction={direction} value={1} style={{ background: WHITE_SMOKE }} />
+          <SliderBar direction={direction} value={value} style={{ background: isEnabled ? GREEN : GRAY }} />
+          <SliderBar direction={direction} value={lastIntent} style={{ background: 'rgba(0, 0, 0, 0.05)' }} />
+          <SliderHandle direction={direction} value={value} style={{ background: isEnabled ? GREEN : GRAY }} />
+        </Slider>
+      </div>
+    </div>
+  )
 }
 
 ReactDOM.render(<ButtonDemo />, document.querySelector('.component-demo[data-component="Button"]'))
