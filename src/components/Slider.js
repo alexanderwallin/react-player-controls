4cd080c56a055850d9b474167c6e0fe63faa562d
Alexander Wallin <office@alexanderwallin.com>
2020-09-21 21:15:58 +0200

    Merge <Slider> and <RangeControlOverlay>

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,9 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useGesture } from 'react-use-gesture'
 
 import { Direction } from '../constants.js'
 import { noop } from '../utils.js'
-import RangeControlOverlay from './RangeControlOverlay.js'
+
+function clamp (value, min, max) {
+  return Math.min(Math.max(value, min), max)
+}
+
+function getRectFromBounds (bounds) {
+  return typeof bounds === 'function' ? bounds() : bounds
+}
+
+function getHorizontalValue (rect, x) {
+  const scrollX = (window.pageXOffset !== undefined)
+    ? window.pageXOffset
+    : (document.documentElement || document.body.parentNode || document.body).scrollLeft
+  const pageX = scrollX + x
+  const dLeft = clamp(pageX - (rect.left + scrollX), 0, rect.width)
+  return dLeft / rect.width
+}
+
+function getVerticalValue (rect, y) {
+  const scrollY = (window.pageYOffset !== undefined)
+    ? window.pageYOffset
+    : (document.documentElement || document.body.parentNode || document.body).scrollTop
+  const pageY = scrollY + y
+  const dTop = clamp(pageY - (rect.top + scrollY), 0, rect.height)
+  return 1 - (dTop / rect.height)
+}
+
+function getSliderValue (bounds, direction, xy) {
+  const rect = getRectFromBounds(bounds)
+  return direction === Direction.HORIZONTAL
+    ? getHorizontalValue(rect, xy[0])
+    : getVerticalValue(rect, xy[1])
+}
 
 /**
  * Slider
@@ -26,37 +59,22 @@ function Slider ({
   overlayZIndex = 10,
 }) {
   const $el = React.createRef()
+  const bounds = () => $el.current.getBoundingClientRect()
 
-  const handleIntent = (intent) => {
-    if (isEnabled) {
-      onIntent(intent)
+  const bind = useGesture(
+    {
+      onMoveStart: ({ dragging, xy }) => isEnabled && !dragging && onIntentStart(getSliderValue(bounds, direction, xy)),
+      onMove: ({ dragging, xy }) => isEnabled && !dragging && onIntent(getSliderValue(bounds, direction, xy)),
+      onMoveEnd: ({ dragging }) => isEnabled && !dragging && onIntentEnd(),
+      onDragStart: ({ xy }) => isEnabled && onChangeStart(getSliderValue(bounds, direction, xy)),
+      onDrag: ({ xy }) => isEnabled && onChange(getSliderValue(bounds, direction, xy)),
+      onDragEnd: ({ xy }) => isEnabled && onChangeStart(getSliderValue(bounds, direction, xy)),
+    },
+    {
+      axis: direction === Direction.HORIZONTAL ? 'x' : 'y',
+      filterTaps: true,
     }
-  }
-  const handleIntentStart = (intent) => {
-    if (isEnabled) {
-      onIntentStart(intent)
-    }
-  }
-  const handleIntentEnd = () => {
-    if (isEnabled) {
-      onIntentEnd()
-    }
-  }
-  const handleChange = (value) => {
-    if (isEnabled) {
-      onChange(value)
-    }
-  }
-  const handleChangeStart = (value) => {
-    if (isEnabled) {
-      onChangeStart(value)
-    }
-  }
-  const handleChangeEnd = (value) => {
-    if (isEnabled) {
-      onChangeEnd(value)
-    }
-  }
+  )
 
   return (
     <div
@@ -69,20 +87,8 @@ function Slider ({
     >
       {children}
 
-      {/*
-        TODO: Make it possible to render or extend this node yourself,
-        so that these styles – the z-index property in particular – is
-        not forced upon the component consumer.
-      */}
-      <RangeControlOverlay
-        direction={direction}
-        bounds={() => $el.current.getBoundingClientRect()}
-        onIntent={handleIntent}
-        onIntentStart={handleIntentStart}
-        onIntentEnd={handleIntentEnd}
-        onChange={handleChange}
-        onChangeStart={handleChangeStart}
-        onChangeEnd={handleChangeEnd}
+      <div
+        {...bind()}
         style={{
           position: 'absolute',
           top: 0,
