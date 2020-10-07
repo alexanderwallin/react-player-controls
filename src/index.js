bbafea53ad37ff523267a2227d6f5cd5a7b217b7
Alexander Wallin <office@alexanderwallin.com>
2020-10-07 22:22:09 +0200

    Remove old Button export

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-// Constants
-// Components
-import Button from './components/Button.js'
 import FormattedTime from './components/FormattedTime.js'
 import * as PlayerIcon from './components/icons.js'
 import Slider from './components/Slider.js'
 
 export { Direction } from './constants.js'
-
-export {
-  Button,
-  FormattedTime,
-  PlayerIcon,
-  Slider,
-}
+export { FormattedTime, PlayerIcon, Slider }
